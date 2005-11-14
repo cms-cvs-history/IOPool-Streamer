c6b0a180ebddd0a526c995bf2f675434be12a4e1
James Kowalkowski <sha1-19a7633f7f2c73cd2080529650f067bdb0f96f44@cern.ch>
2005-11-14 03:34:27 +0000

    add more env variables and another test running script

---
+++ test/MakeTestData.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "LOCAL_TEST_DIR = $LOCAL_TEST_DIR"
+echo "LOCAL_TMP_DIR = $LOCAL_TMP_DIR"
+echo "LOCAL_TOP_DIR = $LOCAL_TOP_DIR"
+echo "LOCAL_TEST_BIN = $LOCAL_TEST_BIN"
+
+cd $LOCAL_TMP_DIR
+
+FILE=${LOCAL_TEST_DIR}/MakeTestData.cfg
+
+sed 's/#R1//g' ${FILE} > ${FILE}_R1
+sed 's/#R2//g' ${FILE} > ${FILE}_R2
+sed 's/#R3//g' ${FILE} > ${FILE}_R3
+
+echo "Generating test data"
+cmsRun --parameter-set ${FILE}_R1 || exit 1
+cmsRun --parameter-set ${FILE}_R2 || exit 1
+cmsRun --parameter-set ${FILE}_R3 || exit 1
+echo "Data written to $LOCAL_TMP_DIR"
+
+${LOCAL_TEST_BIN}/RunFileReader ${FILE}_R1 ${FILE}_R2 ${FILE}_R3
+
