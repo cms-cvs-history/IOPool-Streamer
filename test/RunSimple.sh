41b3bebd847aab35851fcf456ada859e9efef828
James Kowalkowski <sha1-19a7633f7f2c73cd2080529650f067bdb0f96f44@cern.ch>
2005-11-23 02:51:44 +0000

    simple changes

--- test/RunSimple.sh
+++ test/RunSimple.sh
@@ -16,16 +16,24 @@ cd ${OUTDIR}
 
 cmsRun --parameter-set StreamOut.cfg > out 2>&1
 cmsRun --parameter-set StreamIn.cfg  > in  2>&1
+cmsRun --parameter-set StreamCopy.cfg  > copy  2>&1
 
 # echo "CHECKSUM = 1" > out
 # echo "CHECKSUM = 1" > in
 
 ANS_OUT=`grep CHECKSUM out`
 ANS_IN=`grep CHECKSUM in`
+ANS_COPY=`grep CHECKSUM copy`
 
 if [ "${ANS_OUT}" != "${ANS_IN}" ]
 then
-    echo "Simple Stream Test Failed"
+    echo "Simple Stream Test Failed (out!=in)"
+    RC=1
+fi
+
+if [ "${ANS_OUT}" != "${ANS_COPY}" ]
+then
+    echo "Simple Stream Test Failed (copy!=out)"
     RC=1
 fi
 
