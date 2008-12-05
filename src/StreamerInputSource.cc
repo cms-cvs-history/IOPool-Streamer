90a9bf2545643a9989f3d4c7b801798ff7e382c0
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2008-12-05 21:27:37 +0000

    Provenance reduction

--- src/StreamerInputSource.cc
+++ src/StreamerInputSource.cc
@@ -287,7 +287,7 @@ namespace edm {
 
     setRefCoreStreamer(&productGetter_);
     std::auto_ptr<SendEvent> sd((SendEvent*)xbuf_.ReadObjectAny(tc_));
-    setRefCoreStreamer();
+    resetRefCoreStreamer();
 
     if(sd.get()==0) {
         throw cms::Exception("StreamTranslation","Event deserialization error")
