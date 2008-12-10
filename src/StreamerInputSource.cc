a0da8abdade5e54c295deb87c3e0f2b5e8c2927e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2008-12-10 00:28:06 +0000

    Provenance reduction

--- src/StreamerInputSource.cc
+++ src/StreamerInputSource.cc
@@ -236,7 +236,7 @@ namespace edm {
      pset::Registry& psetRegistry = *pset::Registry::instance();
      for (SendJobHeader::ParameterSetMap::const_iterator i = psetMap.begin(), iEnd = psetMap.end(); i != iEnd; ++i) {
        ParameterSet pset(i->second.pset_);
-       pset.fillID();
+       pset.setID(i->first);
        psetRegistry.insertMapped(pset);
      }
   }
